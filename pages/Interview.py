e9f0e09c07142533d9d9c836c7a093b2b8db8664
ark331 <arkhams312@gmail.com>
2025-03-24 14:26:36 +0530

    ..

--- pages/Interview.py
+++ pages/Interview.py
@@ -92,7 +92,7 @@ with tab1:
 
     # Fetch interview questions via Gemini
     def search_on_gemini(role, company, interviewer_type, company_type, difficulty_level):
-        model = genai.GenerativeModel("gemini-2.0-flash")
+        model = genai.GenerativeModel("gemini-1.5-flash")
         prompt = json.load(open("prompts/prompts.json"))
         response = model.generate_content(
             prompt.get('interviewer').format(
